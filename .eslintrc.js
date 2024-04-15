5701b356130d1dc3c50ff45d3c39f89acf5cb3ee
Egorlaptop <egorlavrinovich@gmail.com>
2024-04-15 16:30:41 +0300

    #2.19 add stylint config & i18next rule for eslint

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:i18next/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint',
+        'i18next',
+    ],
+    rules: {
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        indent: [2, 4],
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+        'import/no-unresolved': 'off',
+        'no-unused-vars': 'warn',
+        'no-shadow': 'off',
+        'react/function-component-definition': 'off',
+        'react/require-default-props': 'off',
+        'react/jsx-props-no-spreading': 'warn',
+        'import/prefer-default-export': 'off',
+        'import/extensions': 'off',
+        'import/no-extraneous-dependencies': ['error', {
+            devDependencies: true,
+            optionalDependencies: false,
+            peerDependencies: false,
+        }],
+        'max-len': ['warn', { code: 180 }],
+        'no-unsafe-optional-chaining': 'warn',
+        'no-underscore-dangle': 'off',
+        'no-void': 'off',
+        'no-undef': 'warn',
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
+    },
+    globals: {
+        __IS_DEV__: true,
+    },
+};
